9028f52622661a0da7adcc8c307fc929c56789af
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2022-09-06 13:56:33 +0300

    applied constant variables instead of strings

--- src/views/utils.js
+++ src/views/utils.js
@@ -1,9 +1,17 @@
 import isEmpty from 'lodash/isEmpty';
 
+import {
+  FILTERS,
+  REFERENCES_VALUES_MAP,
+} from '@folio/stripes-authority-components';
+
 export const addDefaultFilters = (searchQuery, filters) => {
   const shouldBeRequest = searchQuery || !isEmpty(filters);
   const defaultFilters = {
-    references: ['excludeSeeFrom', 'excludeSeeFromAlso'],
+    [FILTERS.REFERENCES]: [
+      REFERENCES_VALUES_MAP.excludeSeeFrom,
+      REFERENCES_VALUES_MAP.excludeSeeFromAlso,
+    ],
   };
 
   return {
