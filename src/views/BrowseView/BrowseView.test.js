702773f3e597c8882867594228118f7389592c4f
Dmytro-Melnyshyn <Dmytro_Melnyshyn@epam.com>
2023-05-09 10:50:30 +0300

    add tests

--- src/views/BrowseView/BrowseView.test.js
+++ src/views/BrowseView/BrowseView.test.js
@@ -1,17 +1,23 @@
 import { render } from '@testing-library/react';
 
 import { runAxeTest } from '@folio/stripes-testing';
+import {
+  useAuthoritiesBrowse,
+  useBrowseResultFocus,
+} from '@folio/stripes-authority-components';
 
 import BrowseView from './BrowseView';
 
 import Harness from '../../../test/jest/helpers/harness';
 import AuthoritiesLookup from '../../components/AuthoritiesLookup';
 
+const mockIsPaginationClicked = { current: false };
+const mockHandleLoadMore = jest.fn();
+
 jest.mock('@folio/stripes-authority-components', () => ({
   ...jest.requireActual('@folio/stripes-authority-components'),
-  useBrowseResultFocus: jest.fn().mockReturnValue({
-    resultsContainerRef: { current: null },
-  }),
+  useAuthoritiesBrowse: jest.fn(),
+  useBrowseResultFocus: jest.fn(),
 }));
 
 jest.mock('../../components/AuthoritiesLookup', () => jest.fn(() => <div>AuthoritiesLookup</div>));
@@ -29,6 +35,25 @@ const renderBrowseView = (props = {}) => render(
 );
 
 describe('Given BrowseView', () => {
+  beforeEach(() => {
+    mockIsPaginationClicked.current = false;
+
+    useAuthoritiesBrowse.mockReturnValue({
+      authorities: [],
+      hasNextPage: false,
+      hasPrevPage: false,
+      isLoading: false,
+      isLoaded: false,
+      handleLoadMore: mockHandleLoadMore,
+      query: '(headingRef>="" or headingRef<"") and isTitleHeadingRef==false',
+      totalRecords: 0,
+    });
+    useBrowseResultFocus.mockReturnValue({
+      resultsContainerRef: { current: null },
+      isPaginationClicked: mockIsPaginationClicked,
+    });
+  });
+
   afterEach(() => {
     jest.clearAllMocks();
   });
@@ -57,10 +82,22 @@ describe('Given BrowseView', () => {
       query: '(headingRef>="" or headingRef<"") and isTitleHeadingRef==false',
       resultsContainerRef: { current: null },
       searchQuery: '',
-      totalRecords: NaN,
+      totalRecords: 0,
     };
 
     renderBrowseView();
-    expect(AuthoritiesLookup).toHaveBeenNthCalledWith(2, expectedProps, {});
+    expect(AuthoritiesLookup).toHaveBeenNthCalledWith(1, expectedProps, {});
+  });
+
+  describe('when a user clicks on the pagination button', () => {
+    it('should invoke handleLoadMore and assign isPaginationClicked to true', () => {
+      const args = [100, 95, 0, 'next'];
+
+      renderBrowseView();
+      AuthoritiesLookup.mock.calls[0][0].onNeedMoreData(...args);
+
+      expect(mockHandleLoadMore).toHaveBeenCalledWith(...args);
+      expect(mockIsPaginationClicked.current).toBeTruthy();
+    });
   });
 });
